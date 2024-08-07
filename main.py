0e56fbf21b8dc2d5b95b5168bff43b75613c4328
Shokh Abbos Ulugmurodov <71746304+shohabbosdev@users.noreply.github.com>
2024-08-07 17:30:44 +0000

    second commit

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
+import sox
 import io
 import requests
 import telebot
 from latin_cyrillic_symbols import to_cyrillic
-from pydub import AudioSegment
 from dotenv import dotenv_values
-import os
 
-config = dotenv_values(".env") 
+config = dotenv_values(".env")
 
 bot = telebot.TeleBot(config['token'], parse_mode='HTML')
 
-# Hugging Face API URL va token
 API_URL = "https://api-inference.huggingface.co/models/shohabbosdev/text-to-audio"
 headers = {"Authorization": f"Bearer {config['htoken']}"}
 
@@ -27,8 +25,6 @@ def text_to_speech(text, language="uz"):
         response = requests.post(API_URL, headers=headers, json=payload)
         
         if response.status_code == 200:
-            with open('debug_audio.bin', 'wb') as f:
-                f.write(response.content)            
             return response.status_code, text, response.content
         else:
             return response.status_code, text, None
@@ -38,22 +34,21 @@ def text_to_speech(text, language="uz"):
 
 def convert_to_ogg(audio_content):
     try:
-        with open('temp_audio.flac', 'wb') as f:
-            f.write(audio_content)
-        audio = AudioSegment.from_file('temp_audio.flac', format='flac')
-        audio = audio.set_frame_rate(48000)
+        input_file = io.BytesIO(audio_content)
         temp_file = io.BytesIO()
-        audio.export(temp_file, format="ogg", parameters=["-acodec", "libopus", "-vbr", "on"])
+        
+        # Convert audio using sox
+        tfm = sox.Transformer()
+        tfm.set_output_format(format='ogg', rate=48000)
+        tfm.build_file(input_file, temp_file)
+        
         temp_file.seek(0)
-        os.remove('temp_audio.flac')
-
         return temp_file
     except Exception as e:
         return None
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    # Foydalanuvchi xabar yuborganda "Typing..." belgisi chiqariladi
     bot.send_chat_action(message.chat.id, 'record_voice')
     
     status_code, text, audio_content = text_to_speech(message.text)
@@ -61,11 +56,10 @@ def echo_all(message):
     if status_code == 200 and audio_content:
         ogg_audio = convert_to_ogg(audio_content)
         if ogg_audio:
-            bot.send_voice(message.chat.id, voice=ogg_audio, caption=f'<code>{text}</code>\n\n@shohabbosdev') 
+            bot.send_voice(message.chat.id, voice=ogg_audio, caption=f'<code>{text}</code>\n\n@shohabbosdev')
         else:
             bot.reply_to(message, "Audio faylni konvert qilishda xatolik yuz berdi. Iltimos, dasturchi bilan bog'laning.")
     else:
         bot.reply_to(message, f"Text-to-speech xizmati xatosi: ❌ <code>{status_code}</code>")
-    bot.send_chat_action(message.chat.id, 'record_voice')  
 
 bot.infinity_polling()
